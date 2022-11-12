bcd242976847f8cf6228624c6ccae4044305d3fb
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-12 23:28:23 +0545

    Bottom nav added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:financeapp/utlis/carddetails.dart';
 import 'package:financeapp/utlis/profile.dart';
 import 'package:financeapp/utlis/servies.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,6 +11,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            elevation: 0,
+            backgroundColor: Colors.deepPurple[50],
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.square_grid_2x2), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square), label: "Report"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.creditcard), label: "Billing"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.bell), label: "Notification"),
+            ]),
         backgroundColor: Colors.deepPurple[50],
         body: Padding(
           padding: const EdgeInsets.all(20.0),
